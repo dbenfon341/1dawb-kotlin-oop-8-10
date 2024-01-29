b8d24d98b877c83c1715ddb4d83b6b8b94214ab7
dbenfon341 <dbenfon341@g.educaand.es>
2024-01-29 19:09:56 +0100

    Initial commit.

--- src/Main.kt
+++ src/Main.kt
@@ -8,7 +8,7 @@ el número de páginas y la calificación que le damos entre 0 y 10.
 Posteriormente, crear una clase ConjuntoLibros, que almacena un conjunto de
 libros (con un vector de un tamaño fijo). Se pueden añadir libros que no existan
 (siempre que haya espacio), eliminar libros por título o autor, mostrar por pantalla los
-libros con la mayor y menor calificación y, por último, mostrar un contenido de todo el conjunto.
+libros con la mayor y menor calificación y, por último, mostrar un contenido de tod el conjunto.
 
 En el programa principal realizar las siguientes operaciones: crear dos libros,
 añadirlos al conjunto, eliminarlos por los dos criterios (título y autor) hasta que el
